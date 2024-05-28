59b8bbccab84f017d417d4c976ecff671936f5a1
Lucas Vidotto <lucasvidotto12@gmail.com>
2024-05-28 16:10:39 -0300

    created Models to generate Quiz

--- models.py
+++ models.py
@@ -9,10 +9,32 @@ class User(Base):
     Username = Column(String(50))
     Password = Column(String(200))
     
-
 class Genre(Base):
     __tablename__ = 'TBLGenres'
 
     IDGenre = Column(Integer, primary_key=True, index=True)
     GenreName = Column(String(50))
 
+class StoryType(Base):
+    __tablename__ = 'TBLStoryType'
+
+    IDStoryType = Column(Integer, primary_key=True, index=True)
+    StoryType = Column(String(50))
+
+class AgeMovie(Base):
+    __tablename__ = 'TBLAgeMovie'
+
+    IDAgeMovie = Column(Integer, primary_key=True, index=True)
+    AgeMovie = Column(String(50))
+
+class EndMovie(Base):
+    __tablename__ = 'TBLEndMovie'
+
+    IDEndMovie = Column(Integer, primary_key=True, index=True)
+    EndMovie = Column(String(50))
+
+class KindMovie(Base):
+    __tablename__ = 'TBLKindMovie'
+
+    IDKindMovie = Column(Integer, primary_key=True, index=True)
+    KindMovie = Column(String(50))
